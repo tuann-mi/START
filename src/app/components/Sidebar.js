a15b40e535f2af7514f01fcf3f9e1c0665cba6a1
tuann99 <tnguy256@jhu.edu>
2024-10-06 15:53:17 -0400

    Width reverted to 48

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -11,7 +11,7 @@ export default function Sidebar({ onSelectTables }) {
   };
   const sidebarOptions = ['Dashboard', 'Sites', 'Addresses']
   return (
-    <div id="sidebar" className={`bg-gray-100 dark:bg-gray-800 dark:text-white h-full ${isNotCollapsed ? 'w-50' : 'w-12'} transition-width duration-500 z-40 flex flex-col`}>
+    <div id="sidebar" className={`bg-gray-100 dark:bg-gray-800 dark:text-white h-full ${isNotCollapsed ? 'w-48' : 'w-12'} transition-width duration-500 z-40 flex flex-col`}>
       <div className="flex justify-end p-2">
         <button onClick={toggleSidebar} className="relative right-2">
           {isNotCollapsed ? '◀' : '▶'}
