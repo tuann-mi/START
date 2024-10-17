4bc0a13fe6b1144fba9371755e31f7c0771e9749
tuann99 <tnguy256@jhu.edu>
2024-10-17 16:14:25 -0400

    Prettier formatting

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,42 @@
 "use client";
 
-import Link from 'next/link';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
   const { data: session, status } = useSession();
- 
+
   return (
     <main className="container text-center mx-4">
       <div className="flex-grow flex flex-col items-center justify-center min-w-max">
-        <h1 className="text-6xl font-bold text-gray-900 dark:text-white text-left sm:mb-12 sm:mt-6 md:mb-24">
-            <span className="text-som-primary">Mi</span>chigan<br/>
-            <span className="text-som-primary">E</span>nvironmental<br/>
-            <span className="text-som-primary">H</span>ealth<br/>
-            <span className="text-som-primary">C</span>ommunication &<br/>
-            <span className="text-som-primary">O</span>utreach<br/>
-            <span className="text-som-primary">S</span>ystem
+        <h1 className="text-6xl font-bold text-gray-900 dark:text-white text-left mb-12 s">
+          <span className="text-som-primary">Mi</span>chigan
+          <br />
+          <span className="text-som-primary">E</span>nvironmental
+          <br />
+          <span className="text-som-primary">H</span>ealth
+          <br />
+          <span className="text-som-primary">C</span>ommunication &
+          <br />
+          <span className="text-som-primary">O</span>utreach
+          <br />
+          <span className="text-som-primary">S</span>ystem
         </h1>
         <div className="space-x-8">
-            <Link href="/dashboard" className="bg-som-primary text-white px-6 py-3 rounded-md hover:bg-som-secondary transition duration-300 ease-in-out">
-              Dashboard
-            </Link>
-            <Link href="/calendar" className="bg-som-primary text-white px-6 py-3 rounded-md hover:bg-som-secondary transition duration-300 ease-in-out">
-              Calendar
-            </Link>
+          <Link
+            href="/dashboard"
+            className="bg-som-primary text-white px-6 py-3 rounded-md hover:bg-som-secondary transition duration-300 ease-in-out"
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/calendar"
+            className="bg-som-primary text-white px-6 py-3 rounded-md hover:bg-som-secondary transition duration-300 ease-in-out"
+          >
+            Calendar
+          </Link>
         </div>
       </div>
     </main>
