88d16a1f3696ec78188e35539722d5a21ca3b78f
tuann-mi <nguyent13@michigan.gov>
2024-10-30 10:24:17 -0400

    Added dynamic breadcrumb, removed suspense bc bug

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,7 @@
 "use client";
 
-import { React, Suspense } from "react";
+import React from "react";
+import { usePathname } from "next/navigation";
 import Sidebar from "../components/Sidebar.js";
 import {
   Breadcrumb,
@@ -9,6 +10,42 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+
+function DynamicBreadcrumb() {
+  const pathname = usePathname();
+  const breadcrumbs = pathname
+    .split("/")
+    .filter(Boolean)
+    .map((path, index, array) => {
+      const href = "/" + array.slice(0, index + 1).join("/");
+
+      const title = path
+        .split("-")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+
+      return { title, href };
+    });
+
+  return (
+    <Breadcrumb className="mb-4">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        {breadcrumbs.map((crumb, index) => (
+          <React.Fragment key={crumb.href}>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href={crumb.href}>{crumb.title}</BreadcrumbLink>
+            </BreadcrumbItem>
+          </React.Fragment>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
+
 function DashboardContent({ children }) {
   return (
     <div
@@ -17,17 +54,7 @@ function DashboardContent({ children }) {
     >
       <Sidebar />
       <div id="dashboard-content-container" className="flex-grow p-6 w-96">
-        <Breadcrumb className="mb-4">
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/">Home</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        <DynamicBreadcrumb />
         {children}
       </div>
     </div>
@@ -35,9 +62,5 @@ function DashboardContent({ children }) {
 }
 
 export default function DashboardLayout({ children }) {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <DashboardContent>{children}</DashboardContent>
-    </Suspense>
-  );
+  return <DashboardContent>{children}</DashboardContent>;
 }
